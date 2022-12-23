bceef32281fc60a114a284eeb32d4379e2c9592c
wadekiny <wadekiny@qq.com>
2022-12-23 15:06:11 +0800

    proxy

--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,15 @@
 
 # WSL2
 set -x hostip (cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
-#set -x http_proxy "http://$hostip:7890"
-#set -x https_proxy "http://$hostip:7890"
 set -x all_proxy "socks5://$hostip:7890"
 
+#set -x http_proxy "http://$hostip:7890"
+#set -x https_proxy "http://$hostip:7890"
 #set -x http_proxy "http://172.29.112.1:7890"
 #set -x https_proxy "http://172.29.112.1:7890"
+
+
+
 # WSL2
 
 abbr aconda 'eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv | source'
