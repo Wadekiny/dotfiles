6ff7bb45d2b43e7e6f98f78c3c8a4fb519af274a
wadekiny <wadekiny@qq.com>
2022-12-23 19:56:22 +0800

    cmp-lsp-no-border

--- nvim/subconfig/plugconfig/lsp.lua
+++ nvim/subconfig/plugconfig/lsp.lua
@@ -169,8 +169,8 @@ cmp.setup{
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+    --completion = cmp.config.window.bordered(),
+    --documentation = cmp.config.window.bordered(),
   },
   mapping = {
     ["<Tab>"] = cmp.mapping({
