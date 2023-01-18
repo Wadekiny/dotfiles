98c37da3feec5607ca2a69468a0c34729c541e2f
wadekiny <wadekiny@qq.com>
2023-01-18 18:02:58 +0800

    hyprland

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@
 # !! Contents within this block are managed by 'conda init' !!
 #eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
+set fish_greeting
 abbr aconda 'eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv | source'
 abbr ra ranger
 abbr lg lazygit
