3340d427b1495e9543a1fd18682c044c4391fe61
wadekiny <wadekiny@qq.com>
2023-01-25 18:33:06 +0800

    fish abbr

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,7 @@ abbr aconda 'eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv |
 abbr ra ranger
 abbr lg lazygit
 abbr nv nvim
+abbr vim nvim
 abbr blog cd ~/Blog
 abbr post cd ~/Blog/source/_posts
 abbr todo nvim ~/Blog/source/_posts/TODO.md
