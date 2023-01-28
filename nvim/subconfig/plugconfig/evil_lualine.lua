c6233771d670ca6b396dd29e248c7ea706440d9f
wadekiny <wadekiny@qq.com>
2023-01-28 17:36:48 +0800

    lualine

--- nvim/subconfig/plugconfig/evil_lualine.lua
+++ nvim/subconfig/plugconfig/evil_lualine.lua
@@ -283,7 +283,7 @@ ins_right {
 ins_right {
   'diff',
   -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+  symbols = { added = ' ', modified = '柳', removed = ' ' },
   diff_color = {
     added = { fg = colors.green },
     modified = { fg = colors.orange },
