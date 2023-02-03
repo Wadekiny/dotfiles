33687ac95669544f92d1592288f2d05b203b70dc
wadekiny <wadekiny@qq.com>
2023-02-03 13:03:18 +0800

    add ~/.local/bin to $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@
 #eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 set fish_greeting
+set -x PATH /home/wadekiny/.local/bin $PATH
 abbr aconda 'eval /home/wadekiny/anaconda3/bin/conda "shell.fish" "hook" $argv | source'
 abbr ra ranger
 abbr lg lazygit
