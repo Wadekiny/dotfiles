fac0d015ace231f15eb50c3765629bd1b0d47fee
wadekiny <wadekiny@qq.com>
2023-07-10 00:33:42 +0800

    delete nvim-backup

--- nvim-backup/subconfig/lazy.lua
+++
@@ -1,5 +0,0 @@
-require("lazy").setup({
-    "folke/which-key.nvim",
-    { "folke/neoconf.nvim", cmd = "Neoconf" },
-    "folke/neodev.nvim",
-})
--- nvim-backup/subconfig/plugconfig/telescope.lua
+++
@@ -1,6 +0,0 @@
-require("telescope").setup {
-    defaults = {
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-      --borderchars = {'─', '│', '─', '│', '┌', '┐', '┘', '└'},
-    },
-}
